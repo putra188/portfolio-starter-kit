3dda5a44d81bb65936365b2668ff0191d12feedd
putra188 <femimahardika6@gmail.com>
2025-10-01 12:53:33 +0700

    pesan commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
+   <main>
+       <h1>Halo — saya  Femi Ridho </h1>
+        <p>Ini website sederhana yang saya edit sendiri.</p>
+    <img src="https://data.ecasals.net/img/04/g/9788411581967_04_g.jpg" alt="Hero" />
+  </main>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+        {`I'm a Femi Ridho and Satriyo, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
