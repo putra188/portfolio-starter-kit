2976116a28c994b14033e8e3075fdcb860ece8bc
putra188 <femimahardika6@gmail.com>
2025-10-02 14:13:52 +0700

    menambahkan nama kelompok

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ export default function Page() {
   return (
     <section>
    <main>
-       <h1>Halo — saya  Femi Ridho </h1>
+       <h1>Halo — saya Femi Putra Mahardika </h1>
         <p>Ini website sederhana yang saya edit sendiri.</p>
-    <img src="https://data.ecasals.net/img/04/g/9788411581967_04_g.jpg" alt="Hero" />
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNFJBU32t2i2rHQgtS7ZJtUlO-iZGlvXCEyg&s" alt="Hero" />
   </main>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Femi Ridho and Satriyo, finding unmatched efficiency in
+        {`I'm a Femi, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
